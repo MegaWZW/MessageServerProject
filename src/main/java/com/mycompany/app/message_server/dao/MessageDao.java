dec3c917832621a0c43e6ebaa7e0076e23fc837d
Alexander Sokolovsky <sasha.sokol.04@gmail.com>
2023-01-05 14:04:08 +0300

    Add isUserMessage

--- src/main/java/com/mycompany/app/message_server/dao/MessageDao.java
+++ src/main/java/com/mycompany/app/message_server/dao/MessageDao.java
@@ -1,7 +1,9 @@
 package com.mycompany.app.message_server.dao;
 
 import com.mycompany.app.message_server.core.dto.AcceptedMessageDTO;
+import com.mycompany.app.message_server.core.dto.MessageDTO;
 import com.mycompany.app.message_server.core.dto.RegisteredUserDTO;
+import com.mycompany.app.message_server.core.dto.UserDTO;
 import com.mycompany.app.message_server.dao.api.IMessageDao;
 
 import java.util.ArrayList;
@@ -21,13 +23,32 @@ public class MessageDao implements IMessageDao {
 		String login = registeredUser.getDto().getLogin();
 		List<AcceptedMessageDTO> messagesForUser = new ArrayList<>();
 		for(AcceptedMessageDTO message : messages){
-			if(message.getMessage().getTo().getDto().getLogin().equals(login)){
+
+			if(isUserMessage(message,login)){
 				messagesForUser.add(message);
 			}
 		}
 		return messagesForUser;
 	}
 
+	private boolean isUserMessage(AcceptedMessageDTO message,String login){
+		MessageDTO msgDTO = message.getMessage();
+		if(msgDTO == null)
+			throw new IllegalArgumentException("Сообщение отсутствует");
+		RegisteredUserDTO registeredUserDTO = msgDTO.getTo();
+		if(registeredUserDTO == null)
+			throw new IllegalArgumentException("Адресат письма отсутствет");
+		UserDTO userDTO = registeredUserDTO.getDto();
+		if(userDTO == null)
+			throw new IllegalArgumentException("Адресат не зарегистрирован");
+		String tenpLogin = userDTO.getLogin();
+		if(tenpLogin==null)
+			throw new IllegalArgumentException("У адресата отсутсвует логин");
+
+
+		 login.equals(tenpLogin);
+
+	}
 	@Override
 	public void save(AcceptedMessageDTO message) {
 		this.messages.add(message);
